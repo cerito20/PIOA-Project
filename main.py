1b24606d0262f9684375bb8b79a8b9e7d32da211
cerito20 <certys2007@gmail.com>
2026-05-17 16:56:57 +0500

    Add files via upload

--- main.py
+++ main.py
@@ -5,56 +5,92 @@ from matplotlib.figure import Figure
 from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
 import numpy as np
 import pandas as pd
-from PyQt5.QtWidgets import QMainWindow, QMessageBox, QInputDialog, QLineEdit, QApplication, QWidget, QVBoxLayout, QPushButton, QSizePolicy
+from PyQt5.QtWidgets import QMainWindow, QApplication, QWidget, QVBoxLayout, QPushButton, QListWidget
 from PyQt5.QtCore import Qt
 from PyQt5 import uic
 import sys
 import os
 
+from descriptions import descriptions # импорт descriptions.py
+
 
 def resource_path(relative_path):
+    # Функция которая в будущем понадобится для сборки приложения
     if hasattr(sys, '_MEIPASS'):
         return os.path.join(sys._MEIPASS, relative_path)
     return os.path.join(os.path.abspath("."), relative_path)
 
 
 class MainWindow(QMainWindow):
-    def __init__(self):
+    def __init__(self, dataframe):
         super().__init__()
-        uic.loadUi(resource_path("main.ui"), self)
+        uic.loadUi(resource_path("main.ui"), self) # Загрузка .ui файла (Интерфейс)
 
+        self.df = dataframe # Подключение датасета в класс
 
-        self.fig = Figure()
-        self.canvas = FigureCanvas(self.fig)
-        self.axes = self.fig.add_subplot(111)
-        self.Plot.addWidget(self.canvas)
+        self.fig = Figure()                   # 
+        self.canvas = FigureCanvas(self.fig)  # Добавление холста для графика
+        self.axes = self.fig.add_subplot(111) # 
+        self.Plot.addWidget(self.canvas)      # 
 
-        self.btn_show.clicked.connect(self.show_category)
-        self.btn_back.clicked.connect(self.back_to_menu)
-
-    def show_category(self):
-        self.lb_category.setText(self.cb_categories.currentText())
-        self.update_plot()
-        self.stackedWidget.setCurrentIndex(1)
+        self.btn_show.clicked.connect(self.show_category) #
+        self.btn_back.clicked.connect(self.back_to_menu)  # Подключение кнопок
+        self.btn_inspect.clicked.connect(self.inspect)    #
 
     def back_to_menu(self):
+        # Возвращает на главную страницу
         self.stackedWidget.setCurrentIndex(0)
 
-    def update_plot(self):
-        self.axes.clear()
-        self.axes.bar([1, 2, 3, 4, 6], np.random.randint(low=0, high=100, size=5))
-        self.canvas.draw()
-        
+    def show_category(self):
+        # Переопределяет всю информацию и переходит на вторую страницу
+        category = self.cb_categories.currentText() # Получение названия выбранной пользователем категории
+        self.lb_category.setText(category) # Изменение верхнего текста на второй странице на название выбранной категории
+        self.lb_info.setText(descriptions[category]) # Изменение описания категории
+        self.update_bar([1, 2, 3, 4, 5], np.random.randint(low=0, high=100, size=5)) # Вызов функции, которая обновляет график (Параметры пока что случайные)
+        self.update_list(self.categories(category)) # Вызов функции, которая обновляет список
+        for i in range(self.table.rowCount()):     # 
+                table_item = self.table.item(i, 1) # Очистка таблицы от прошлых значений
+                table_item.setText(' ')            # 
+        self.stackedWidget.setCurrentIndex(1) # Переход на вторую страницу
+
+    def categories(self, category):
+        # Возвращает отсортированный список по заранее заданным критериям по категориям.
+        match(category):
+            case 'Спорт': filtered = self.df.loc[(self.df['acceleration'] > 10) & (self.df['horsepower'] > 200)] # Фильтры пока что случайные
+            # Сюда в будущем добавлять остальные категории
+        return filtered
+    
+    def inspect(self):
+        # Обновляет таблицу для выбранного из списка автомобиля
+        current_item = self.list.currentItem() # Получение выбранного предмета списка
+        if current_item: # Проверка на None 
+            car_name = current_item.text() # Получение текстового значения выбранного предмета списка (Название автомобиля)
+            row = df[df['name'] == car_name].to_numpy().tolist()[0] # Получение массива с данными выбранного автомобиля
+            for i in range(self.table.rowCount()): # 
+                table_item = self.table.item(i, 1) # Заполнение таблицы полученными данными
+                table_item.setText(str(row[i]))    # 
+
+
+    def update_bar(self, x, height):
+        # Обновляет график
+        self.axes.clear() # Очистка от прошлый значений
+        self.axes.barh(x, height) # Задание новых значений
+        self.canvas.draw() # Отрисовка графика
+
+    def update_list(self, new_list):
+        # Обновляет список
+        self.list.clear() # Очистка от прошлых значений
+        self.list.addItems(new_list['name']) # Добавление новых значений
 
 if __name__ == "__main__":
-    app = QApplication(sys.argv)
-    window = MainWindow()
-    window.show()
-    sys.exit(app.exec_())
+    df = kagglehub.dataset_load(                     # 
+        KaggleDatasetAdapter.PANDAS,                 # 
+        "whenamancodes/automobiles-project-dataset", #  Загрузка датасета
+        "Automobile.csv",                            # 
+        pandas_kwargs={"encoding": "cp1252"},        # 
+    )                                                # 
 
-
-    # df = kagglehub.dataset_load(
-    #     KaggleDatasetAdapter.PANDAS,
-    #     "krupalpatel07/nvidia-historical-data",
-    #     "NVDA.csv",
-    # )
+    app = QApplication(sys.argv) # 
+    window = MainWindow(df)      # Запуск окна
+    window.show()                # 
+    sys.exit(app.exec_())        # 
