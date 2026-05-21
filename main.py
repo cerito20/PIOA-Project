ba623415a425724b31bb1eaeab3aeba8ae971893
cerito20 <certys2007@gmail.com>
2026-05-21 22:03:56 +0500

    Add files via upload

--- main.py
+++ main.py
@@ -34,28 +34,71 @@ class MainWindow(QMainWindow):
         self.axes = self.fig.add_subplot(111) # 
         self.Plot.addWidget(self.canvas)      # 
 
-        self.btn_show.clicked.connect(self.show_category) #
-        self.btn_back.clicked.connect(self.back_to_menu)  # Подключение кнопок
-        self.btn_inspect.clicked.connect(self.inspect)    #
+        # Подключение кнопок
+        self.btn_show.clicked.connect(lambda: self.pick_category('Без категории'))
+        self.btn_category_1.clicked.connect(lambda: self.pick_category('Спорт'))
+        self.btn_category_2.clicked.connect(lambda: self.pick_category('2'))
+        self.btn_category_3.clicked.connect(lambda: self.pick_category('3'))
+        self.btn_category_4.clicked.connect(lambda: self.pick_category('4'))
+        self.btn_goto_LowPrice.clicked.connect(lambda: self.pick_price((10, 20)))
+        self.btn_goto_MidPrice.clicked.connect(lambda: self.pick_price((20,30)))
+        self.btn_goto_HighPrice.clicked.connect(lambda: self.pick_price((30,40)))
+        self.btn_back.clicked.connect(lambda: self.switch_pages(back=True))
+        self.btn_back_2.clicked.connect(lambda: self.switch_pages(back=True))
+        self.btn_menu.clicked.connect(lambda: self.switch_pages(menu=True))
+        self.btn_inspect.clicked.connect(self.inspect)
 
-    def back_to_menu(self):
-        # Возвращает на главную страницу
-        self.stackedWidget.setCurrentIndex(0)
+    def switch_pages(self, forward=False, back=False, menu=False):
+        # Меняет страницу в зависимости от переданных переменных
+        if forward: # Вперёд
+            current = self.stackedWidget.currentIndex()
+            self.stackedWidget.setCurrentIndex(current+1)
+        if back: # Назад
+            current = self.stackedWidget.currentIndex()
+            self.stackedWidget.setCurrentIndex(current-1)
+        if menu: # Меню
+            self.stackedWidget.setCurrentIndex(0)
+    
+    def pick_category(self, category):
+        # Записывает выбранную категорию в формате str (Для отсутствия категории - отдельный функционал)
+        self.category = category
+        if category == 'Без категории':
+            self.price = (0, 40) # Здесь нужно установить цену от минимальной до максиальной (Вручную)
+            self.btn_back_2.hide()                   # 
+            self.btn_menu.move(50, 30)               # 
+            self.lb_info.hide()                      # Перестройка сцены
+            self.lb_list.move(50 ,110)               # 
+            self.list.setGeometry(50, 170, 300, 540) # 
+            self.show_category() # Т.к выводится весь список, страница с ценами пропускается
+            self.switch_pages(forward=True) # 2 раза переходим на следующую страницу
+            self.switch_pages(forward=True) # (чтобы не создавать отдельный случай)
+        else:
+            self.lb_info.show()
+            self.btn_back_2.show()                    # 
+            self.btn_menu.move(210, 30)               # Перестройка сцены
+            self.lb_list.move(50 ,430)                # 
+            self.list.setGeometry(50, 490, 300, 220)  # 
+            self.switch_pages(forward=True) # Переход на следующую страницу
+    
+    def pick_price(self, price):
+        # Записывает выбранную ценовую категорию в формате tuple(low, high)
+        self.price = price
+        self.show_category() # Вызов функции которая выводит 3 страницу со всеми данными
+        self.switch_pages(forward=True) # Переход на следующую страницу
 
     def show_category(self):
-        # Переопределяет всю информацию и переходит на вторую страницу
-        category = self.cb_categories.currentText() # Получение названия выбранной пользователем категории
-        self.lb_category.setText(category) # Изменение верхнего текста на второй странице на название выбранной категории
-        self.lb_info.setText(descriptions[category]) # Изменение описания категории
+        # Переопределяет всю информацию
+
+        self.lb_category.setText(self.category) # Изменение верхнего текста на третьей странице на название выбранной категории
+        self.lb_price.setText(f'${self.price[0]}    —    ${self.price[1]}')
+        self.lb_info.setText(descriptions[self.category]) # Изменение описания категории
         self.update_bar([1, 2, 3, 4, 5], np.random.randint(low=0, high=100, size=5)) # Вызов функции, которая обновляет график (Параметры пока что случайные)
-        self.update_list(self.categories(category)) # Вызов функции, которая обновляет список
+        self.update_list(self.categories(self.category)) # Вызов функции, которая обновляет список
         for i in range(self.table.rowCount()):     # 
                 table_item = self.table.item(i, 1) # Очистка таблицы от прошлых значений
                 table_item.setText(' ')            # 
         self.photo.clear() # Очистка прошлой фотографии
-        self.lb_carName.clear() # Очистка прошлого названия
         self.photo.setToolTip("")
-        self.stackedWidget.setCurrentIndex(1) # Переход на вторую страницу
 
     def categories(self, category):
         # Возвращает отсортированный список по заранее заданным критериям по категориям.
@@ -73,8 +116,7 @@ class MainWindow(QMainWindow):
             row = df[df['name'] == car_name].to_numpy().tolist()[0] # Получение массива с данными выбранного автомобиля
             for i in range(1, self.table.rowCount()+1): # 
                 table_item = self.table.item(i-1, 1)    # Заполнение таблицы полученными данными
-                table_item.setText(str(row[i]))         # 
-            self.lb_carName.setText(car_name) # Отрисовка названия автомобиля
+                table_item.setText(str(row[i]))         #
         
             pixmap = QPixmap(f"cars_photos/{car_name}.png") # Загрузка фотографии машины из папки cars_photos (на фотографии ford f250 другая машина)
             if pixmap.isNull(): # Проверка на Null
